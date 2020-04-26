9c7ec2708a8fba15a12ee1899244a3b8b0a02991
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-26 05:26:23 -0300

    feat(theme): define default app theme props

--- src/styles/theme.style.js
+++ src/styles/theme.style.js
@@ -1,5 +1,35 @@
+import chroma from 'chroma-js';
+
+const colors = {
+  primaryLight: chroma('#2796FB'),
+  primary: chroma('#007DFA'),
+  primaryDark: chroma('#0065DC'),
+
+  secondaryLight: chroma('#2796FB'),
+  secondary: chroma('#007DFA'),
+  secondaryDark: chroma('#0065DC'),
+
+  dark: chroma('#212121'),
+  darkest: chroma('#000000'),
+
+  light: chroma('#EDEDED'),
+  lightest: chroma('#FFFFFF'),
+};
+
 export default {
-  colors: {},
-  sizes: {},
-  weights: {},
+  colors,
+  shadows: {
+    sm: `0 1px 2px ${colors.darkest.alpha(0.24)}`,
+    md: `0 2px 4px ${colors.darkest.alpha(0.24)}`,
+  },
+  sizes: {
+    h1: '3.2rem',
+    h2: '2.4rem',
+    h3: '2rem',
+    p: '1.6rem',
+  },
+  weights: {
+    normal: '400',
+    strong: '700',
+  },
 };
