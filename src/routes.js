2dc99375ae583e7a46c95001407ef83f5f2b7c18
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-26 05:30:56 -0300

    feat(routes): define not found page route, update redirect
    - Fix route path params
    - Define not found page route
    - Set redirect to take the user to not found page

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
-import { Photos, Posts } from './pages';
+import { Photos, Posts, NotFound } from './pages';
 
 export default function () {
   return (
-    <>
-      <Route to="/:userId/photos" component={Photos} />
-      <Route to="/:userId/posts" component={Posts} />
-      <Redirect to="/:userId/photos" />
-    </>
+    <Switch>
+      <Route path="/:userId/photos" component={Photos} />
+      <Route path="/:userId/posts" component={Posts} />
+
+      <Route path="/erro/nao-encontrado" component={NotFound} />
+
+      <Redirect to="/erro/nao-encontrado" />
+    </Switch>
   );
 }
