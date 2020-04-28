8f3763b4a7200f7eb65be4628f0ebe389ea419f0
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-28 07:29:23 -0300

    feat(gallery): implement multiple pages behavior

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -9,6 +9,7 @@ import * as S from './styled';
 const INITIAL_STATE = { id: '', title: '', url: '' };
 function Gallery({ photos }) {
   const [active, setActive] = useState({ ...INITIAL_STATE });
+  const [page, setPage] = useState({ actual: 1, total: 1 });
 
   function onThumbClickHandler(photoId) {
     const { title, url } = photos.find(({ id }) => id === photoId);
@@ -16,13 +17,25 @@ function Gallery({ photos }) {
   }
 
   function onControlClickHandler(action) {
-    console.log(action);
+    const { actual, total } = page;
+
+    if (action === 'prev') {
+      if (actual === 1) return setPage((prev) => ({ ...prev, actual: total }));
+      return setPage((prev) => ({ ...prev, actual: actual - 1 }));
+    }
+
+    if (actual === total) return setPage((prev) => ({ ...prev, actual: 1 }));
+    return setPage((prev) => ({ ...prev, actual: actual + 1 }));
   }
 
   useEffect(() => {
-    if (photos.length && !active.url) {
-      const { id, title, url } = photos[0];
-      setActive({ id, title, url });
+    if (photos.length) {
+      if (!active.url) {
+        const { id, title, url } = photos[0];
+        setActive({ id, title, url });
+      }
+
+      setPage({ actual: 1, total: Math.ceil(photos.length / 4) });
     }
   }, [photos]);
 
@@ -35,16 +48,18 @@ function Gallery({ photos }) {
       />
 
       <Controls onControlClick={onControlClickHandler}>
-        {photos.map(({ id, thumbnailUrl, title }) => (
-          <Thumb
-            active={id === active.id}
-            id={id}
-            key={id}
-            title={title}
-            url={thumbnailUrl}
-            onClick={() => onThumbClickHandler(id)}
-          />
-        ))}
+        {photos
+          .slice((page.actual - 1) * 4, page.actual * 4)
+          .map(({ id, thumbnailUrl, title }) => (
+            <Thumb
+              active={id === active.id}
+              id={id}
+              key={id}
+              title={title}
+              url={thumbnailUrl}
+              onClick={() => onThumbClickHandler(id)}
+            />
+          ))}
       </Controls>
     </S.GalleryGrid>
   );
