f5906c5856fb922a48b6721de279f847e3c66c76
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-29 09:07:09 -0300

    feat(stories-utils): add a component to a button with link style

---
+++ src/stories/utils/StoryLink.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background: none;
+  border: none;
+  color: #337ab7;
+  cursor: pointer;
+  font-size: 14px;
+  margin: 4px 0 0 0;
+  transition: color 400ms;
+  &:hover {
+    color: #29639e;
+  }
+`;
