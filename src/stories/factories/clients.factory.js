723211f3b73cb139d8f83709722928478cc5a22d
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-29 09:09:00 -0300

    feat(stories-factories): add address prop to client factory method

--- src/stories/factories/clients.factory.js
+++ src/stories/factories/clients.factory.js
@@ -5,8 +5,21 @@ export function single({
   email = faker.internet.email(),
   id = faker.random.uuid(),
   name = faker.name.findName(),
+  address = {},
 } = {}) {
-  return { company, email, id, name };
+  return {
+    company,
+    email,
+    id,
+    name,
+    address: {
+      street: faker.address.streetAddress(),
+      suite: faker.address.secondaryAddress(),
+      city: faker.address.city(),
+      zipcode: faker.address.zipCode(),
+      ...address,
+    },
+  };
 }
 
 export function list(seeds = 5) {
