8b7f62a7fffd94b73867077de418d425d057cfcf
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-29 18:59:20 -0300

    feat(posts-page): define posts page markup

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { Client, Post } from '../../stories/factories';
+
+import { Layout, PageGrid } from '../../components';
+import ClientDetails from '../../components/ClientDetails';
+import PostsList from '../../components/PostsList';
+import ClientsList from '../../components/ClientsList';
+
 function PostsPage() {
-  return <section>Posts Page</section>;
+  return (
+    <Layout title="Posts">
+      <PageGrid>
+        <ClientsList clients={Client.list(8)} />
+        <ClientDetails client={Client.single()}>
+          <PostsList posts={Post.list(24)} />
+        </ClientDetails>
+      </PageGrid>
+    </Layout>
+  );
 }
 
 export default PostsPage;
