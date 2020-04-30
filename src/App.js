05eaa0efb9b89d31c2d51f40a331640e7e4f600f
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-04-30 09:32:19 -0300

    feat(root-component): wrap my app with combined contexts
    - Import AppContextProvider from contexts
    - Wrap app root component with AppContextProvider to make all contexts available to my components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Routes from './routes';
+import AppContextProvider from './contexts';
 import { GlobalStyle, Theme } from './styles';
 
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={Theme}>
-        <GlobalStyle />
-        <Routes />
-      </ThemeProvider>
+      <AppContextProvider>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyle />
+          <Routes />
+        </ThemeProvider>
+      </AppContextProvider>
     </BrowserRouter>
   );
 }
