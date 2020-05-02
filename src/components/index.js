7433f8159250722d136f454c19470e7c9609c499
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-02 03:06:52 -0300

    feat(components-barrel): export new components
    - export client details
    - export clients list
    - export gallery

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+export { default as ClientDetails } from './ClientDetails';
+export { default as ClientsList } from './ClientsList';
+export { default as Gallery } from './Gallery';
 export { default as Layout } from './Layout';
 export { default as Loader } from './Loader';
 export { default as PageGrid } from './PageGrid';
