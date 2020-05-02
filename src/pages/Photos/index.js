2aa8069f46ca50082f366d61ce59811e824bd4db
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-02 03:10:18 -0300

    feat(photos-page): integrate with clients context

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
-import { Gallery as GalleryFactory, Client } from '../../stories/factories';
+import { $clients } from '../../services';
+import { Gallery as GalleryFactory } from '../../stories/factories';
 
-import ClientsList from '../../components/ClientsList';
-import ClientDetails from '../../components/ClientDetails';
-import Gallery from '../../components/Gallery';
-import { Layout, PageGrid } from '../../components';
+import {
+  ClientDetails,
+  ClientsList,
+  Gallery,
+  Layout,
+  PageGrid,
+} from '../../components';
+import { useClients } from '../../contexts';
 
 function PhotosPage() {
+  const [activeClient, setActiveClient] = useState({});
+
+  const { clients } = useClients();
+  const { userId } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!clients.length) $clients.fetchAll();
+  }, []);
+
+  useEffect(() => {
+    let [active] = clients;
+    const found = clients.find((client) => client.id === userId);
+    if (found) active = found;
+
+    if (Object.keys(active || {}).length) setActiveClient(active);
+  }, [clients]);
+
+  useEffect(() => {
+    const { id: newUserId = 1 } = activeClient;
+    history.push(`/${newUserId}/fotos`);
+  }, [activeClient]);
+
+  useEffect(() => {
+    const { id: activeUserId = 1 } = activeClient;
+    const intUserId = parseInt(userId, 10);
+
+    if (intUserId && activeUserId !== intUserId) {
+      const client = clients.find((each) => each.id === intUserId);
+      setActiveClient(client);
+    }
+  }, [userId]);
+
   return (
     <Layout title="Fotos">
       <PageGrid>
-        <ClientsList clients={Client.list(16)} />
-        <ClientDetails client={Client.single()}>
-          <Gallery photos={GalleryFactory.list(11)} />
-        </ClientDetails>
+        {Object.keys(activeClient || {}).length ? (
+          <>
+            <ClientsList clients={clients} />
+            <ClientDetails client={activeClient}>
+              <Gallery photos={GalleryFactory.list(11)} />
+            </ClientDetails>
+          </>
+        ) : null}
       </PageGrid>
     </Layout>
   );
