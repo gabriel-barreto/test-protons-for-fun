96acd78d2084e51fecd6e3d3e0c895071d20e6cb
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-02 08:21:35 -0300

    feat(components-barrel): export not found illustration component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as ClientsList } from './ClientsList';
 export { default as Gallery } from './Gallery';
 export { default as Layout } from './Layout';
 export { default as Loader } from './Loader';
+export { default as NotFoundIllustration } from './NotFoundIllustration';
 export { default as PageGrid } from './PageGrid';
