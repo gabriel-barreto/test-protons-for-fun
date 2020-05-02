fd2823be2fbd7b78acce1b018ff8d00e9d1e44da
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-02 02:52:02 -0300

    feat(services): create an axios instance
    - create an axios instance with default values
    - register interceptors
    - enable when request somehting
    - disable loader when get a response

---
+++ src/services/http.service.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import PubSub from 'pubsub-js';
+
+import { LoaderContextTopic } from '../contexts';
+
+const $http = axios.create({
+  baseURL: 'http://jsonplaceholder.typicode.com',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
+
+$http.interceptors.request.use((config) => {
+  PubSub.publish(LoaderContextTopic, true);
+  return config;
+});
+
+$http.interceptors.response.use(
+  (res) => {
+    PubSub.publish(LoaderContextTopic, false);
+    return res;
+  },
+  (err) => {
+    PubSub.publish(LoaderContextTopic, false);
+    throw err.response;
+  },
+);
+
+export default $http;
