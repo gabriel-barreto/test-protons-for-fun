ba6b4fe4a8a4552a8f8febd8f9838a9a1c446161
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-04 22:03:41 -0300

    feat(layout): update paddings and sizes on smaller screens

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
-export const Layout = styled.main`
-  background-color: ${({ theme }) => theme.colors.dark.hex()};
-  height: 100vh;
-  overflow: hidden;
-`;
+import { $breakpoints } from '../../utils';
 
 export const Container = styled.div`
-  padding: 16rem 6.4rem 3.2rem 6.4rem;
+  padding: 16rem 3.2rem 3.2rem 3.2rem;
+
+  @media screen and (min-width: ${$breakpoints('lg')}) {
+    padding: 16rem 6.4rem 3.2rem 6.4rem;
+  }
+`;
+
+export const Layout = styled.main`
+  background-color: ${({ theme }) => theme.colors.dark.hex()};
+  overflow: hidden;
+
+  @media screen and (min-width: ${$breakpoints('lg')}) {
+    height: 100vh;
+  }
 `;
