57b8b0d2de8f951b502a8560e2af06f8992323ad
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-05-04 22:01:13 -0300

    feat(contexts): apply active user context to app context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { ActiveClientContextProvider } from './activeClient.context';
 import { ClientContextProvider } from './clients.context';
 import { LoaderContextProvider } from './loader.context';
 import { PhotosContextProvider } from './photos.context';
@@ -8,21 +9,21 @@ import { PostsContextProvider } from './posts.context';
 
 export default function AppContext({ children }) {
   return (
-    <ClientContextProvider>
-      <LoaderContextProvider>
-        <PhotosContextProvider>
-          <PostsContextProvider>{children}</PostsContextProvider>
-        </PhotosContextProvider>
-      </LoaderContextProvider>
-    </ClientContextProvider>
+    <ActiveClientContextProvider>
+      <ClientContextProvider>
+        <LoaderContextProvider>
+          <PhotosContextProvider>
+            <PostsContextProvider>{children}</PostsContextProvider>
+          </PhotosContextProvider>
+        </LoaderContextProvider>
+      </ClientContextProvider>
+    </ActiveClientContextProvider>
   );
 }
 AppContext.propTypes = { children: PropTypes.node.isRequired };
 
+export { useActiveClient } from './activeClient.context';
 export { useClients } from './clients.context';
 export { useLoader } from './loader.context';
 export { usePhotos } from './photos.context';
 export { usePosts } from './posts.context';
-
-export { CONTEXT_TOPIC as LoaderContextTopic } from './loader.context';
-export { CONTEXT_TOPIC as ClientContextTopic } from './clients.context';
